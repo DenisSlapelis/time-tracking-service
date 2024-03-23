8c52ea6f3573fa3d0f18befbed0d03b4e4b12e4a
colantuomo <pcolantuomo@gmail.com>
2024-03-22 23:21:38 -0300

    feat: init all envs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
+import { env } from '@env';
 import { app } from './app';
 import serverlessExpress from '@codegenie/serverless-express';
 
+env.populateAllEnvs();
 export const handler = serverlessExpress({ app });
